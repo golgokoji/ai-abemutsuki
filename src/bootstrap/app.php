dd6343fdb45e049c8152993ee7bcd434832aa697
Koji Ueda <golgokoji@gmail.com>
2025-10-08 21:58:49 +0900

    honban

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
